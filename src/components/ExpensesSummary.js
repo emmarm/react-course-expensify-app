8c71caccaed247812f35dda485a59f4b79f4e7e3
Emma M <mdopure@gmail.com>
2018-02-14 21:33:19 +0700

    Added ExpensesSummary test and fixes

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,11 +4,17 @@ import getVisibleExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-const ExpensesSummary = (props) => (
-  <div>
-    <p>Viewing {props.expenses.length} expense{props.expenses.length !== 1 ? 's' : ''} totalling {numeral(getExpensesTotal(props.expenses)).format('$0,0.00')}</p>
-  </div>
-);
+export const ExpensesSummary = (props) => {
+  const numberOf = props.expenses.length;
+  const s = props.expenses.length !== 1 ? 's' : '';
+  const total = getExpensesTotal(props.expenses);
+  const formattedTotal = numeral(total).format('$0,0.00');
+  return (
+    <div>
+      <p>Viewing {numberOf} expense{s} totalling {formattedTotal}</p>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   expenses: getVisibleExpenses(state.expenses, state.filters)
